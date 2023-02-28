d4f4e701095a0e1c70d499fe640ad002d6306bcd
Najmus Sakib <106301767+itsnajmus@users.noreply.github.com>
2023-02-28 17:21:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const evaluatedText = document.getElementById('evaluatedText');
+const wordCount = document.getElementById('wordCount');
+
+evaluatedText.addEventListener('input', () => {
+  const text = evaluatedText.value.trim();
+  const count = text ? text.split(/\s+/).length : 0;
+  wordCount.innerText = count;
+});
